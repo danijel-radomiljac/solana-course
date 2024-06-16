c6c1920e6c336fe18354062dc08c2110eaca0e6d
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-06-16 20:21:48 +0200

    module 1 of solana dev course

---
+++ module1.ts
@@ -0,0 +1,21 @@
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from '@solana/web3.js';
+
+async function getBalanceUsingWeb3(address: PublicKey): Promise<number> {
+  const connection = new Connection(clusterApiUrl('devnet'));
+
+  return connection.getBalance(address);
+}
+
+async function main() {
+  const address = new PublicKey('Dany9vz3TQLPqdJmNQJp7cqnBpJThMSsLwHjVsLovUG8');
+  const balance = (await getBalanceUsingWeb3(address)) / LAMPORTS_PER_SOL;
+
+  console.log(balance + ' SOL');
+}
+
+main();
